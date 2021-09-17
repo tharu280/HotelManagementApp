32d07d92bcfa2b29bcbb5f1da4eb2bd305e280bf
KalpaAdithya <89289299+KalpaAdithya@users.noreply.github.com>
2021-09-18 01:32:19 +0530

    Update Checkin.cs

--- Checkin.cs
+++ Checkin.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Loginpage
 {
-    public partial class Checkinform : Form
+    public partial class Delete : Form
     {
-        public Checkinform()
+        public Delete()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = int.parse(taxID.Text);
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalID)\MSSQLLocalDB");
+            string DelQry = "DELETE FROM Infotable WHERE ID_Number ="+id+"";
+            SqlCommand cmd = new SqlCommand(DelQry,con);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessgeBox.show("Data Delete Successfuly");
+            }
+            catch(SqlException se)
+            {
+                MessgeBox.show("Error occured" + se.Tostring());
+            }
+            finaly
+            {
+               con.close();
+            }
         }
     }
 }
