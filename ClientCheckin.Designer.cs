65f6114dc0c1e5415da0857e0fbfbbc62227492a
Pasindu10 <89079667+Pasindu10@users.noreply.github.com>
2021-09-18 18:27:38 +0530

    Update ClientCheckin.Designer.cs

--- ClientCheckin.Designer.cs
+++ ClientCheckin.Designer.cs
@@ -29,10 +29,17 @@ namespace Loginpage
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ClientCheckin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1201, 763);
+            this.Name = "ClientCheckin";
             this.Text = "ClientCheckin";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
